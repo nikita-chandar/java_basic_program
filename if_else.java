01a063417eb7dc90626ec2f0405dc5be763e5b13
nikita-chandar <nikitachandar99@gmail.com>
2022-11-08 14:43:55 +0530

    removed code and formating

--- if_else.java
+++ if_else.java
@@ -2,42 +2,29 @@ package com.company;
 import java.util.Scanner;
 public class if_else
 {
- public static void main(String args[])
-{
-   Scanner in=new Scanner(System.in);
-System.out.println("enter the age");
- int age=in.nextInt();
-Scanner obj=new Scanner(System.in);
-System.out.println("Enter the name");
-String name=obj.nextLine();
-Scanner myobj=new Scanner(System.in);
-System.out.println("Enter the salary");
-double salary=myobj.nextDouble();
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter the age");
+		int age=in.nextInt();
+		Scanner obj=new Scanner(System.in);
+				System.out.println("Enter the name");
+		String name=obj.nextLine();
+		Scanner myobj=new Scanner(System.in);
+		System.out.println("Enter the salary");
+		double salary=myobj.nextDouble();
+        System.out.println("age:" +age);
+		System.out.println("name:" +name);  
+		System.out.println("salary:" +salary);
+			if(age>18)
+			{
+				System.out.println("ram has permission to drive the car");
 
-   
-
-
-
-
-System.out.println("age:" +age);
-
-
-System.out.println("name:" +name);  
-
-System.out.println("salary:" +salary);
-if(age>18)
-{
-System.out.println("ram has permission to drive the car");
-
-}
-else
-{
-  System.out.println("ram does not have permission to drive the car");
-}
-  
-
-}
-
-
+			}
+			else
+			{
+				System.out.println("ram does not have permission to drive the car");
+			}
+	}
 
 }
