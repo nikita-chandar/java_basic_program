e836cca97d9fee00c58d74bb94f71daf575f19e9
nikita-chandar <117675495+nikita-chandar@users.noreply.github.com>
2022-11-08 14:30:54 +0530

    SumOfArray

---
+++ sum_of_array.java
@@ -0,0 +1,21 @@
+
+//import java.util.Scanner;
+class demo
+{
+   public static void main(String[] args)
+   {
+	   int[] arr={5,7,9,2,1};
+	  // Scanner sc=new Scanner(System.in);
+	  // int arr=sc.nextInt();
+	  int sum=0;
+	   
+	   
+	   for(int i=0;i<arr.length;i++)
+	   {
+			 sum=arr[i]+sum;
+	   }
+	   System.out.println(sum);
+	   
+   }
+
+}
