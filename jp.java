02c4bc22aa05cd06b0c0070ff6175386346c1c3d
nikita-chandar <nikitachandar99@gmail.com>
2022-11-16 23:39:02 +0530

    final key

---
+++ jp.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+	
+public class pwh_ch2
+{
+  public static void main(String[] args)
+{
+  System.out.println("what is your name");
+  Scanner sc= new Scanner(System.in);
+  String name=sc.next();
+  System.out.println("hello"+ name +"have a good day");
+  
+}
+}
---
+++ loop.java
@@ -0,0 +1,25 @@
+//wap to print a natural no:
+package com.company;
+ public class loop
+{
+public static void main(String[] args){
+ int i=100;
+System.out.println("NATURAL NO IS:");
+while(i<=200)
+{
+System.out.println(i);
+i++;
+
+//System.out.println("USE LOOP:");
+// i=1;
+//while(i<=3)
+//{
+  //System.out.println(i);
+//i++;
+  
+//while(1)
+//{
+  //System.out.println("I am an infinite loop");
+}
+}
+}
